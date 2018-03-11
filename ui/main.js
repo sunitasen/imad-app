cb8d1c7baecaa2cda5e5afeff4fd456dc3c9ca83
sunitasen <36556702+sunitasen@users.noreply.github.com>
2018-03-11 21:46:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,21 @@
 var button=document.getElementById("counter");
-var counter=0;
+
 button.onclick= function(){
+    var request=new XMLhttpRequest();
+    request.onreadystatechange= function(){
+        if(request.readystate===XMLhttpRequest.DONE){
+        
+            if(request.status===200){
+                var counter =request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
     
-    counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
-    
+            }
+        }
+        
+
+    };
+   
+   request.open('GET','http://sunitacool41.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
